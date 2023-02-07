4c90bc42b72d052efc187d30fb959e20b3e5d23b
giuliagrfo <giuliagrifo1@gmail.com>
2023-02-07 12:42:22 +0100

    Create views

---
+++ src/views/PlatesView.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+    name: 'Plates'
+}  
+</script>
+
+<template>
+
+</template>
---
+++ src/views/RestaurantsView.vue
@@ -0,0 +1,9 @@
+<script>
+export default {
+    name: 'Restaurants'
+}  
+</script>
+
+<template>
+
+</template>
