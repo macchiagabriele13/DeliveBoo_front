e2e4bdcaff01deadbe2b51a014a6c04b244f14c6
Marco Inchingoli <evilmarkus@hotmail.it>
2023-02-09 17:15:42 +0100

    Conctacts Fixed, Add Effect onHover

--- src/views/ContactsView.vue
+++ src/views/ContactsView.vue
@@ -23,14 +23,29 @@ export default {
             <!-- assistenza ordini online -->
             <div class="row">
                 <h2>Assistenza ordini</h2>
-                <div class="col-4 border-end border-2 mt-3 p-3 mb-5">
-                    <div class="online_order_number">
+                <div class="col-4 border-end border-2 mt-2 p-3 mb-5 d-flex">
+                    <div class="phone_image d-flex align-items-center me-3 pb-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="40" fill="currentColor" class="bi bi-phone"
+                            viewBox="0 0 16 16">
+                            <path
+                                d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z" />
+                            <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+                        </svg>
+                    </div>
+                    <div class="online_order_number lh-1 d-flex flex-column justify-content-center">
                         <p>Chiamaci</p>
                         <h5>0000-123-456789</h5>
                     </div>
                 </div>
-                <div class="col-4 ms-4">
-                    <div class="online_order_mail mt-3 p-3 mb-5">
+                <div class="col-4 ms-4 mt-2 p-3 mb-5 d-flex">
+                    <div class="mail_image d-flex align-items-center me-3 pb-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="40" fill="currentColor" class="bi bi-envelope"
+                            viewBox="0 0 16 16">
+                            <path
+                                d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" />
+                        </svg>
+                    </div>
+                    <div class="online_order_mail lh-1 d-flex flex-column justify-content-center">
                         <p>Inviaci una mail</p>
                         <h5>info@example.com</h5>
                     </div>
@@ -44,24 +59,47 @@ export default {
             </div>
         </div>
         <div
-            class="container d-flex justify-content-center mt_custom flex-column align-items-center border-bottom border-2 w-75">
+            class="container d-flex justify-content-center mt-5 flex-column align-items-center border-bottom border-2 w-75">
             <!-- ritiro in negozio -->
             <div class="row">
                 <h2>Ritiro in negozio</h2>
-                <div class="col-4 border-end border-2 mt-3 p-3 mb-5">
-                    <div class="store_pickup_number">
+                <div class="col-4 border-end border-2 mt-2 p-3 mb-5 d-flex">
+                    <div class="phone_image d-flex align-items-center me-3 pb-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="40" fill="currentColor" class="bi bi-phone"
+                            viewBox="0 0 16 16">
+                            <path
+                                d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z" />
+                            <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+                        </svg>
+                    </div>
+                    <div class="store_pickup_number lh-1 d-flex flex-column justify-content-center">
                         <p>Chiamaci</p>
                         <h5>0000-123-456789</h5>
                     </div>
                 </div>
-                <div class="col-4 ms-4 border-end border-2 mt-3 p-3 mb-5">
-                    <div class="store_pickup_mail">
+                <div class="col-4 ms-4 border-end border-2 mt-2 p-3 mb-5 d-flex">
+                    <div class="mail_image d-flex align-items-center me-3 pb-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="40" fill="currentColor" class="bi bi-envelope"
+                            viewBox="0 0 16 16">
+                            <path
+                                d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z" />
+                        </svg>
+                    </div>
+                    <div class="store_pickup_mail lh-1 d-flex flex-column justify-content-center">
                         <p>Inviaci una mail</p>
                         <h5>info@example.com</h5>
                     </div>
                 </div>
-                <div class="col-3 ms-4">
-                    <div class="store_pickup_address mt-3 p-3 mb-5">
+                <div class="col-3 ms-4 mt-2 p-3 mb-5 d-flex">
+                    <div class="address_image d-flex align-items-center me-3 pb-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="40" fill="currentColor" class="bi bi-geo-alt"
+                            viewBox="0 0 16 16">
+                            <path
+                                d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
+                            <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                        </svg>
+                    </div>
+                    <div class="store_pickup_address lh-1 d-flex flex-column justify-content-center">
                         <p>Noi siamo qui</p>
                         <h5>No: 58 A, East Madison Street, Baltimore, MD, USA 4508</h5>
                     </div>
@@ -117,6 +155,14 @@ export default {
 <style lang="scss" scoped>
 .bg_orange {
     background-color: orange;
+}
+
+.col-3:hover,
+.col-4:hover {
+    svg {
+        color: orange;
+        cursor: default;
+    }
 }
 
 .fs_custom {
