0d6da93e4dd580ea1ebc0325e64da332fa614649
macchiagabriele13 <macchia_gabriele@libero.it>
2023-02-11 01:52:55 +0100

    Add cart-view, cart-comp, check-view

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <script>
 import AppNav from './components/AppNav.vue'
 import AppFooter from './components/AppFooter.vue'
+import Cart from './components/Cart.vue'
 
 export default {
     components: {
         AppNav,
         AppFooter,
+        Cart,
     }
 }
 </script>
 
 <template>
     <AppNav />
+    <Cart />
     <RouterView />
     <AppFooter />
 </template>
