b275e127bd632c6404309c01ae309ed54d499d26
Marco Inchingoli <evilmarkus@hotmail.it>
2023-02-13 18:23:36 +0100

    Add dynamic banner in SingleRestaurantView

--- src/views/SingleRestaurantView.vue
+++ src/views/SingleRestaurantView.vue
@@ -1,9 +1,36 @@
 <script>
+import axios from 'axios'
 import PlateList from '../components/PlateList.vue'
 export default {
     name: 'SingleRestaurant',
     components: {
         PlateList,
+    },
+    data() {
+        return {
+            restaurant: [],
+            base_api_url: 'http://localhost:8000',
+        }
+    },
+    methods: {
+        getRestaurant(url) {
+            axios
+                .get(url)
+                .then(response => {
+                    console.log(response.data.results);
+                    this.restaurant = response.data.results;
+                    this.loading = false
+                })
+                .catch(error => {
+                    console.error(error)
+                    this.error = error.message
+
+                })
+        },
+    },
+    mounted() {
+        console.log(this.$route.params.slug)
+        this.getRestaurant(this.base_api_url + '/api/restaurants/' + this.$route.params.slug);
     }
 }  
 </script>
@@ -13,8 +40,12 @@ export default {
     <section id="banner_single_restaurant">
         <div class="container-fluid bg_orange">
             <div class="container d-flex align-items-center justify-content-center justify-content-around">
-                <h1 class="fs_custom">Ristorante</h1>
-                <img height="400" src="../../public/img/pac-man-contacts.png" alt="">
+                <h1 class="fs_custom">{{ restaurant.company_name }}</h1>
+                <div class="card bg_orange border-0 d-flex" style="height: 20rem; width: 40rem;">
+                    <div class="card-body restaurant_details d-flex justify-content-center flex-column">
+                        <p class="card-text fs-3">{{ restaurant.description }}</p>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
