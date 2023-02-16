59e326b16a6e587780c37cc3ec11c4249e9518bf
macchiagabriele13 <macchia_gabriele@libero.it>
2023-02-16 12:47:16 +0100

    modifiche varie

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router'
-
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 /* import the fontawesome core */
