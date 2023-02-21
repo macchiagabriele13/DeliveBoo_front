d498ee26364039fb5d42d150ed4c24f0d0633066
giuliagrfo <giuliagrifo1@gmail.com>
2023-02-21 13:44:09 +0100

    Fix restaurant card responsive

--- src/components/RestaurantCard.vue
+++ src/components/RestaurantCard.vue
@@ -112,7 +112,6 @@ export default {
     overflow: hidden;
 }
 
-@media screen {}
 
 .container .card:before {
     content: '';
@@ -239,9 +238,9 @@ export default {
     cursor: pointer;
 }
 
-// .container .card .contentBx .size span:hover {
-//     background: $primary_soft;
-// }
+.container .card .contentBx .size span:hover {
+    background: $primary_soft;
+}
 
 .container .card .contentBx .color span {
     width: 90px;
@@ -266,5 +265,135 @@ export default {
     transform: translateY(0px);
     transition-delay: 0.75s;
 
+}
+
+
+
+// #########################################
+
+
+@media screen and (max-width:425px) {
+    .container .card {
+        position: relative;
+        width: 320px;
+        height: 450px;
+        background: $light;
+        border-radius: 20px;
+        overflow: hidden;
+    }
+
+    .container .card:before {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: $primary;
+        clip-path: circle(150px at 80% 20%);
+        transition: 0.5s ease-in-out;
+    }
+
+    .container .card .imgBx {
+        position: absolute;
+        top: 30%;
+        transform: translateY(-50%);
+        z-index: 2;
+        width: 100%;
+        height: 220px;
+        transition: 0.5s;
+    }
+
+    .container .card .imgBx img {
+        position: absolute;
+        top: 0;
+        left: 20%;
+        width: 200px;
+        height: 200px;
+        border-radius: 50%;
+        object-fit: cover;
+        object-position: center;
+    }
+
+    .container .card .contentBx {
+        position: absolute;
+        bottom: 50px;
+        width: 100%;
+        height: 100px;
+        text-align: center;
+
+    }
+
+    .container .card:hover .contentBx {
+        height: 210px;
+    }
+
+    .container .card .contentBx h2 {
+        position: relative;
+        font-weight: 600;
+        letter-spacing: 1px;
+        color: $primary;
+        margin: 0;
+    }
+
+    .container .card .contentBx .size,
+    .container .card .contentBx .color {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        opacity: 1;
+        visibility: visible;
+        padding-top: 0;
+        padding-bottom: 0;
+    }
+
+
+    .container .card .contentBx .size h3,
+    .container .card .contentBx .color h3 {
+        color: $dark;
+        font-weight: 300;
+        font-size: 14px;
+        text-transform: uppercase;
+        letter-spacing: 2px;
+        margin-right: 10px;
+    }
+
+    .container .card .contentBx .size span {
+        width: 26px;
+        height: 26px;
+        text-align: center;
+        line-height: 26px;
+        font-size: 14px;
+        display: inline-block;
+        color: #111;
+        background: #fff;
+        margin: 0 5px;
+        // transition: 0.5s;
+        color: #111;
+        border-radius: 4px;
+        cursor: pointer;
+    }
+
+    .container .card .contentBx .size span:hover {
+        background: $primary_soft;
+    }
+
+    .container .card .contentBx .color span {
+        width: 90px;
+        height: 20px;
+        background: $primary_soft;
+        border-radius: 5px;
+        margin: 0 5px;
+        cursor: default;
+    }
+
+    .contentBx {
+
+        h2,
+        h3,
+        .div {
+            cursor: default;
+        }
+    }
 }
 </style>
